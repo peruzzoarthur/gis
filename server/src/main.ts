3607c0a3140c67abd26fb43c09439897cce2f73c
sp3ruzzo <peruzzoarthur@gmail.com>
2024-07-18 12:39:35 -0300

    trying to fix issue

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,12 +6,12 @@ import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  // const corsOptions: CorsOptions = {
-  //   origin: "gis.railway.internal",
-  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  //   credentials: true,
-  // };
-  // app.enableCors(corsOptions);
+  const corsOptions: CorsOptions = {
+    origin: "https://gis.up.railway.app:8080", // Replace with your actual front-end URL
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
