dc333bfd4c7bc42a1439f45e6b84b7746d15ddcd
sp3ruzzo <peruzzoarthur@gmail.com>
2024-07-18 19:56:47 -0300

    changed title at html

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,16 +6,16 @@ import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  // const corsOptions: CorsOptions = {
-  //   origin: "https://gis.up.railway.app",
-  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  //   credentials: true,
-  // };
   const corsOptions: CorsOptions = {
-    origin: process.env.CORS_ORIGIN,
+    origin: "https://gis.up.railway.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   };
+  // const corsOptions: CorsOptions = {
+  //   origin: process.env.CORS_ORIGIN,
+  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //   credentials: true,
+  // };
   app.enableCors(corsOptions);
   await app.listen(3000);
 }
